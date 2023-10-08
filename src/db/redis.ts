1259d115b55df2d5a8e4c84136ec931ccc76cef9
Rodrigo <rodrigo.noschang1@gmail.com>
2023-10-08 01:27:07 -0300

    Changed .env file for redis connection

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -2,7 +2,7 @@ import Redis from 'ioredis';
 
 import { env } from '../env';
 
-export const redis = new Redis(env.REDIS_URL, {
+export const redis = new Redis(env.REDIS_TLS_URL, {
     tls: {
         rejectUnauthorized: false
     }
--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const envSchema = z.object({
     DATABASE_URL: z.string(),
     JWT_SECRET: z.string(),
-    REDIS_URL: z.string()
+    REDIS_TLS_URL: z.string()
 });
 
 export const env = envSchema.parse(process.env);

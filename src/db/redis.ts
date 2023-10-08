e90ccca5991d99738b29fe3f12baffd9871c7a15
Rodrigo <rodrigo.noschang1@gmail.com>
2023-10-08 01:21:18 -0300

    try: fixing .env again

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -2,12 +2,8 @@ import Redis from 'ioredis';
 
 import { env } from '../env';
 
-export const redis = new Redis(
-    env.REDISCLOUD_PORT,
-    env.REDISCLOUD_URL,
-    {
-        connectTimeout: 10000,
-        port: env.REDISCLOUD_PORT,
-        host: env.REDISCLOUD_URL
+export const redis = new Redis(env.REDIS_URL, {
+    tls: {
+        rejectUnauthorized: false
     }
-);
+});
--- src/env/index.ts
+++ src/env/index.ts
@@ -4,9 +4,7 @@ import 'dotenv/config';
 const envSchema = z.object({
     DATABASE_URL: z.string(),
     JWT_SECRET: z.string(),
-    PORT: z.coerce.number().default(4000),
-    REDISCLOUD_URL: z.string(),
-    REDISCLOUD_PORT: z.coerce.number().default(6379)
+    REDIS_URL: z.string()
 });
 
 export const env = envSchema.parse(process.env);

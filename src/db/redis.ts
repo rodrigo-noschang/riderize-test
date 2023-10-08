9da8b78e7d5f203570cf9a08b3a4b91a1bd021dd
Rodrigo <rodrigo.noschang1@gmail.com>
2023-10-08 01:00:44 -0300

    try: Changin REDISURL and PORT to .env file

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -2,4 +2,12 @@ import Redis from 'ioredis';
 
 import { env } from '../env';
 
-export const redis = new Redis(6379, env.REDISCLOUD_URL);
+export const redis = new Redis(
+    env.REDISCLOUD_PORT,
+    env.REDISCLOUD_URL,
+    {
+        connectTimeout: 10000,
+        port: env.REDISCLOUD_PORT,
+        host: env.REDISCLOUD_URL
+    }
+);
--- src/env/index.ts
+++ src/env/index.ts
@@ -5,7 +5,8 @@ const envSchema = z.object({
     DATABASE_URL: z.string(),
     JWT_SECRET: z.string(),
     PORT: z.coerce.number().default(4000),
-    REDISCLOUD_URL: z.string()
+    REDISCLOUD_URL: z.string(),
+    REDISCLOUD_PORT: z.coerce.number().default(6379)
 });
 
 export const env = envSchema.parse(process.env);
